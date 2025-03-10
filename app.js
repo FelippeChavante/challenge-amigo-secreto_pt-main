86ea5be261ba8f58b6815db591f06c9bfd336c7e
Felippe Chavant <felippechavante@gmail.com>
2025-03-10 10:12:02 -0300

    Codigo sorteador

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-
 let amigos = [];
+
+function adcionarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value;
+
+    if (!nomeAmigo) {
+        alert("Digite o nome do amigo");
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizalista();
+}
+
+function atualizarlista(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerhtml = "";
+
+    for(let i = 0; i , amigos.length; i++){
+        let item - document.createElement("li");
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    }
+}
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("Nenhum amigo adcionado");
+        return;
+    }
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resutaldo.innerHTML = `O amigo sorteado foi ${sorteado}`;
+
+    let limparLista = document.getElementById("listaAmigos");
+    limparLista.innerHTML = "";
+    amigos = [];
+}
